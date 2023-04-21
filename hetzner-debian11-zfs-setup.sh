2e4c8d3a32c71597745d2edbb4d8f42ee4e63c43
terem42 <9478806+terem42@users.noreply.github.com>
2023-04-21 08:34:07 +0200

    update source repo for installing zfs from backports

--- hetzner-debian11-zfs-setup.sh
+++ hetzner-debian11-zfs-setup.sh
@@ -712,7 +712,7 @@ if [[ $v_zfs_experimental == "1" ]]; then
   chroot_execute "apt update"
   chroot_execute "apt install -t zfs-debian-experimental --yes zfs-initramfs zfs-dkms zfsutils-linux"
 else
-  chroot_execute "apt install --yes zfs-initramfs zfs-dkms zfsutils-linux"
+  chroot_execute "apt install -t bullseye-backports --yes zfs-initramfs zfs-dkms zfsutils-linux"
 fi
 chroot_execute 'cat << DKMS > /etc/dkms/zfs.conf
 # override for /usr/src/zfs-*/dkms.conf:
