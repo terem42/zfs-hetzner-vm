9e3847f28d8e349a5be017b2c6ea43e40754d036
terem42 <9478806+terem42@users.noreply.github.com>
2023-10-04 13:44:25 +0200

    updated wih testing

--- hetzner-debian12-zfs-setup.sh
+++ hetzner-debian12-zfs-setup.sh
@@ -498,15 +498,14 @@ echo "======= installing zfs on rescue system =========="
   echo "zfs-dkms zfs-dkms/note-incompatible-licenses note true" | debconf-set-selections
   apt-get install --yes software-properties-common
 #  echo "y" | zfs
-#  zfs --version
-  apt update
+#  zfs --version 
   apt install --yes dpkg-dev dkms linux-headers-generic linux-image-generic
   rm "$(which zfs)"
   export PATH=$PATH:/usr/sbin
   echo -e "deb http://deb.debian.org/debian/ testing main contrib non-free\ndeb http://deb.debian.org/debian/ testing main contrib non-free\n" >/etc/apt/sources.list.d/bookworm-testing.list
   echo -e "Package: src:zfs-linux\nPin: release n=testing\nPin-Priority: 990\n" > /etc/apt/preferences.d/90_zfs
   apt update
-  apt install -t testing zfs-dkms zfsutils-linux
+  apt install -t testing --yes zfs-dkms zfsutils-linux
   rm /etc/apt/sources.list.d/bookworm-testing.list
   rm /etc/apt/preferences.d/90_zfs
   apt update
