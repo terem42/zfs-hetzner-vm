587d3a1f4f4e9ec914ff8b693b9bac447ca84d3b
Cong Zhang <13283869+congzhangzh@users.noreply.github.com>
2025-04-22 05:01:11 +0800

    fix grub install error (#75)
    Co-authored-by: Nemo <9478806+terem42@users.noreply.github.com>

--- hetzner-debian12-zfs-setup.sh
+++ hetzner-debian12-zfs-setup.sh
@@ -559,21 +559,7 @@ echo "======= create zfs pools and datasets =========="
 # shellcheck disable=SC2086
 zpool create \
   -o cachefile=/etc/zpool.cache \
-  -o ashift=12 -d \
-  -o feature@async_destroy=enabled \
-  -o feature@bookmarks=enabled \
-  -o feature@embedded_data=enabled \
-  -o feature@empty_bpobj=enabled \
-  -o feature@enabled_txg=enabled \
-  -o feature@extensible_dataset=enabled \
-  -o feature@filesystem_limits=enabled \
-  -o feature@hole_birth=enabled \
-  -o feature@large_blocks=enabled \
-  -o feature@lz4_compress=enabled \
-  -o feature@spacemap_histogram=enabled \
-  -o feature@zpool_checkpoint=enabled \
-  -O acltype=posixacl -O canmount=off -O compression=lz4 \
-  -O devices=off -O normalization=formD -O relatime=on -O xattr=sa \
+  -o compatibility=grub2 \
   -O mountpoint=/boot -R $c_zfs_mount_dir -f \
   $v_bpool_name $pools_mirror_option "${bpool_disks_partitions[@]}"
 
